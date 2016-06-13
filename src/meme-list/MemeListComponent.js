97af69a3b00fad94560169811687be5f181026d6
Jan Doležal <jan.dolezal@firma.seznam.cz>
2016-06-13 15:33:02 +0200

    Reposition meme contents

--- src/meme-list/MemeListComponent.js
+++ src/meme-list/MemeListComponent.js
@@ -1,17 +1,24 @@
 import React from 'react';
 
+import './index.css';
+
 export default function MemeList (props) {
 	const memes = props.items.map((item) => {
 		return (
-			<li className="collection-item avatar">
-				<img
-					key={item.id}
-					width="100"
-					height="100"
-					src={item.url}
-				/>
-				<strong className="title">Tags:</strong>
-				<p>{item.tags.join(', ')}</p>
+			<li key={item.id} className="collection-item avatar">
+				<div className="meme">
+					<div className="meme__inner-column">
+						<img
+							width="100"
+							height="100"
+							src={item.url}
+						/>
+					</div>
+					<div className="meme__inner-column">
+						<strong className="title">Tags:</strong>
+						<p>{item.tags.join(', ')}</p>
+					</div>
+				</div>
 			</li>
 		);
 	});
